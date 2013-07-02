4c4366883846a7050265f2c53a490aa9167163d1
Huiming Teo <teohuiming@gmail.com>
2013-07-02 21:50:58 +0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Fulamak::Application.config.secret_key_base = '6be2ce43b930376ec80a7690fb8b9cc97666cbb72ec11638574606603b80dc6b12b3bbcad65b00932e9420d5f89c1c53db1194214b7be5f1dcb258ed506fc981'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Fulamak::Application.config.session_store :cookie_store, key: '_fulamak_session'
